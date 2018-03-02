fc6fb3c8f8b6b1f84a076794549930d7c71cdcb9
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-03-02 20:27:29 +0700

    init client

--- goredis.go
+++ goredis.go
@@ -1,24 +1,26 @@
 package goredis
 
 import (
-	"github.com/go-redis/redis"
 	"bitbucket.org/subiz/map"
-	"fmt"
 	"errors"
+	"fmt"
+	"github.com/go-redis/redis"
+	"strconv"
 	"time"
 )
 
 type ShardRedisClient Client
 
 type Client struct {
-	locks cmap.Map
+	locks   cmap.Map
 	clients cmap.Map
+	shard   int
 }
 
-func (c *Client) connectTo(redishost, password string) error {
+func (c *Client) connectTo(i int, redishost, password string) error {
 	client := redis.NewClient(&redis.Options{
-			Addr:     redishost,
-			Password: password,
+		Addr:     redishost,
+		Password: password,
 	})
 	pong, err := client.Ping().Result()
 	if err != nil {
@@ -27,21 +29,37 @@ func (c *Client) connectTo(redishost, password string) error {
 	}
 	fmt.Println("pong from " + redishost + ": " + pong)
 	c.locks.Set(redishost, client)
+	c.clients.Set(strconv.Itoa(i), client)
 	return nil
 }
 
+func fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	const prime32 = uint32(16777619)
+	for i := 0; i < len(key); i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
+	}
+	return hash
+}
+
+func (c *Client) GetKey(key string) string {
+	return strconv.Itoa(int(fnv32(key)) % c.shard)
+}
+
 func (c *Client) Connect(hosts []string, password string) error {
+	c.shard = len(hosts)
 	c.locks = cmap.New(len(hosts) * 2) // decrease collition rate of key
 	c.clients = cmap.New(len(hosts) * 2)
 	reschan := make(chan error, len(hosts))
-	for _, host := range hosts {
-		go func(host string) {
-			reschan <- c.connectTo(host, password)
-		}(host)
+	for i, host := range hosts {
+		go func(i int, host string) {
+			reschan <- c.connectTo(i, host, password)
+		}(i, host)
 	}
 
 	for range hosts {
-		err := <- reschan
+		err := <-reschan
 		if err != nil {
 			return err
 		}
@@ -50,7 +68,7 @@ func (c *Client) Connect(hosts []string, password string) error {
 }
 
 func (c *Client) Get(shardkey, key string) ([]byte, error) {
-	clienti, ok := c.clients.Get(shardkey)
+	clienti, ok := c.clients.Get(c.GetKey(shardkey))
 	if !ok {
 		return nil, errors.New("should not hapend, client is not init")
 	}
@@ -60,7 +78,7 @@ func (c *Client) Get(shardkey, key string) ([]byte, error) {
 }
 
 func (c *Client) Set(shardkey, key string, value []byte, dur time.Duration) error {
-	clienti, ok := c.clients.Get(shardkey)
+	clienti, ok := c.clients.Get(c.GetKey(shardkey))
 	if !ok {
 		return errors.New("should not hapend, client is not init")
 	}
@@ -69,7 +87,7 @@ func (c *Client) Set(shardkey, key string, value []byte, dur time.Duration) erro
 }
 
 func (c *Client) Expire(shardkey, key string, dur time.Duration) error {
-	clienti, ok := c.clients.Get(shardkey)
+	clienti, ok := c.clients.Get(c.GetKey(shardkey))
 	if !ok {
 		return errors.New("should not hapend, client is not init")
 	}
