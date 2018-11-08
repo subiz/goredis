e895b4e538271505a32e6bbd53a946c0e13abb23
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-11-08 14:52:13 +0700

    return not found if not found

--- goredis.go
+++ goredis.go
@@ -76,9 +76,6 @@ func (c *Client) Get(shardkey, key string) ([]byte, error) {
 	client := clienti.(*redis.Client)
 	str, err := client.Get(key).Bytes()
 	if err != nil {
-		if err.Error() == redis.Nil.Error() {
-			return nil, nil
-		}
 		return nil, err
 	}
 	return str, nil
